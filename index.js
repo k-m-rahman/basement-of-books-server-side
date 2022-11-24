126f4ad8fd093ed8a108445d79118f83fa455d88
k-m-rahman <k_m_rahman23@hotmail.com>
2022-11-24 10:54:00 +0600

    added database and users api

--- index.js
+++ index.js
@@ -13,6 +13,51 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// connection of the database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.siwxcfo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    // users collection
+    const usersCollection = client.db("basementOfBooks").collection("users");
+
+    //-----------------------
+    // API for users
+    //-----------------------
+
+    // getting all the users
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const users = await usersCollection.find(query).toArray();
+      res.send(users);
+    });
+
+    // posting an user
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      // checking whether the user is already in the DB
+      const query = { email: user.email };
+      const alreadyUser = await usersCollection.findOne(query);
+
+      if (alreadyUser) {
+        return res.send({ message: "This email is already in use" });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Basement of books server is running");
 });
